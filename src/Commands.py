dd66a0c79be991d499b4e3245e3dc1ed7aa2692a
Enzo Bonato <enzo.bonato@epitech.eu>
2022-11-17 11:36:47 +0100

    check error

--- src/Commands.py
+++ src/Commands.py
@@ -16,7 +16,22 @@ class Commands:
             'ABOUT': self.about,
         }
 
+    def check_param(nb_param, params):
+        if nb_param == 4:
+            if (len(params) != nb_param - 2):
+                return False
+        else:
+            new_list = params[0].split(' ')
+            if (new_list[0] == ''):
+                if (nb_param != 0):
+                    return False
+            elif (len(new_list) != nb_param):
+                return False  
+        return True
+
     def start(self, params, game: Game, logic: Logic):
+        if (Commands.check_param(1, params) == False):
+            return
         if (int(params[0]) < 5):
             print(f'ERROR Board size incorrect', flush=True)
             return
@@ -24,17 +39,23 @@ class Commands:
         print('OK', flush=True)
 
     def turn(self, params, game: Game, logic: Logic):
+        if (Commands.check_param(4, params) == False):
+            return
         game.fillBoard(int(params[0]), int(params[1]), '2')
         x, y = logic.getBestMove(game)
         game.fillBoard(x, y, '1')
         print(f'{x},{y}', flush=True)
 
     def begin(self, params, game: Game, logic: Logic):
+        if (Commands.check_param(0, params) == False):
+            return
         x = game.getBoardSize() / 2
         y = game.getBoardSize() / 2
         print(f'{x},{y}', flush=True)
 
     def board(self, params, game: Game, logic: Logic):
+        if (Commands.check_param(0, params) == False):
+            return
         Parse = Parser()
         Parse.askForInput()
 
@@ -49,7 +70,12 @@ class Commands:
         return
 
     def about(self, params, game: Game, logic: Logic):
-        print('about', flush=True)
+        if (Commands.check_param(0, params) == False):
+            return
+        print('name="Best IA", version="1.0", author="The Group", country="France"', flush=True)
 
     def executeCommand(self, command, params, game: Game, logic: Logic):
+        if (command != "START" and command != "TURN" and command != "BEGIN" and command != "BOARD" and command != "INFO" and command != "ABOUT"):
+            print("Command not found")
+            return
         self.commands[command](params, game, logic)
