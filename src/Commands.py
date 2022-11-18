04fdc86f3713dfada4a023d40c9c592b5789dde2
Enzo Bonato <enzo.bonato@epitech.eu>
2022-11-18 11:04:31 +0100

    [EB][ADD] Add display function check error and check if case already taken

--- src/Commands.py
+++ src/Commands.py
@@ -14,9 +14,13 @@ class Commands:
             'BOARD': self.board,
             'INFO': self.info,
             'ABOUT': self.about,
+            'DISPLAY': self.display,
         }
+        self.size_x = 0
+        self.size_y = 0
+        self.is_start = False
 
-    def check_param(nb_param, params):
+    def check_param(nb_param, params) -> bool:
         if nb_param == 4:
             if (len(params) != nb_param - 2):
                 return False
@@ -35,18 +39,40 @@ class Commands:
         if (int(params[0]) < 5):
             print(f'ERROR Board size incorrect', flush=True)
             return
+        self.size_x = int(params[0])
+        self.size_y = int(params[0])
+        self.is_start = True
         game.initBoard(int(params[0]))
         print('OK', flush=True)
 
+    def check_coordinate(self, params, game: Game, logic: Logic):
+        if (int(params[0]) >= self.size_x or int(params[1]) >= self.size_y):
+            print("Coordinate out of range", flush=True)
+            return False
+        if (Game.check_valid_case(game, int(params[0]), int(params[1])) == False):
+            return False
+        return True
+
     def turn(self, params, game: Game, logic: Logic):
+        if (self.is_start == False):
+            print("Game not start", flush=True)
         if (Commands.check_param(4, params) == False):
+            return
+        if (Commands.check_coordinate(self, params, game, logic) == False):
             return
         game.fillBoard(int(params[0]), int(params[1]), '2')
         x, y = logic.getBestMove(game)
         game.fillBoard(x, y, '1')
         print(f'{x},{y}', flush=True)
 
+    def display(self, params, game: Game, logic: Logic):
+        if (self.is_start == False):
+            print("Game not start", flush=True)
+        print(game.board)
+
     def begin(self, params, game: Game, logic: Logic):
+        if (self.is_start == False):
+            print("Game not start", flush=True)
         if (Commands.check_param(0, params) == False):
             return
         x = game.getBoardSize() / 2
@@ -54,6 +80,8 @@ class Commands:
         print(f'{x},{y}', flush=True)
 
     def board(self, params, game: Game, logic: Logic):
+        if (self.is_start == False):
+            print("Game not start", flush=True)
         if (Commands.check_param(0, params) == False):
             return
         Parse = Parser()
@@ -75,7 +103,7 @@ class Commands:
         print('name="Best IA", version="1.0", author="The Group", country="France"', flush=True)
 
     def executeCommand(self, command, params, game: Game, logic: Logic):
-        if (command != "START" and command != "TURN" and command != "BEGIN" and command != "BOARD" and command != "INFO" and command != "ABOUT"):
-            print("Command not found")
+        if (command != "START" and command != "TURN" and command != "BEGIN" and command != "BOARD" and command != "INFO" and command != "ABOUT" and command != "DISPLAY"):
+            print("Command not found", flush=True)
             return
         self.commands[command](params, game, logic)
--- src/Game.py
+++ src/Game.py
@@ -2,16 +2,22 @@
 
 class Game:
     def __init__(self):
-        self.board = None
+        self.board = []
 
     def initBoard(self, size):
         self.board = [[' ' for x in range(size)] for y in range(size)]
 
     def fillBoard(self, x, y, player):
-        self.board[y][x] = player
+        self.board[x][y] = player
 
     def printBoard(self):
         print(self.board)
 
     def getBoardSize(self):
         return (len(self.board))
+
+    def check_valid_case(game, x, y) -> bool:
+        if (game.board[x][y] != ' ' or game.board[x][y] != ' '):
+            print("Case already taken", flush=True)
+            return False
+        return True
