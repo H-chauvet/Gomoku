c379c8fec34c3edda66455a7820e7d4ce07eed12
Enzo Bonato <enzo.bonato@epitech.eu>
2022-11-21 14:38:36 +0100

    [EB][ADD] Find all diagonal with 4 up to 5 and fill it

--- src/Logic.py
+++ src/Logic.py
@@ -23,6 +23,24 @@ class Logic:
                     self.x = x
                     self.y = y
                     return True
+        elif self.whichPattern == 3:
+            x -= 5
+            y -= 4
+            for x in range(x, x + 5):
+                if game.board[x][y] == ' ':
+                    self.x = x
+                    self.y = y
+                    return True
+                y += 1
+        elif self.whichPattern == 4:
+            x -= 5
+            y += 5
+            for x in range(x, x + 5):
+                if game.board[x][y] == ' ':
+                    self.x = x
+                    self.y = y
+                    return True
+                y -= 1
         return False
 
     def finPatternDeffense(self, game: Game, x, y) -> bool:
@@ -53,10 +71,49 @@ class Logic:
                 counter = 0
                 self.whichPattern = 0
 
-
         # Diagonal A
-        # for i in range(0, x - 4):
-        #     for j in range(0, y - 4):
+        i = 0
+        while (i != x - 4):
+            for j in range(0, y - 4):
+                for j in range(j, j + 5):
+                    if game.board[i][j] == '2':
+                        counter += 1
+                    i += 1
+                if (counter == 4):
+                    self.whichPattern = 3
+                    if self.findDefense(game, i, j) == True:
+                        return True
+                counter = 0
+                self.whichPattern = 0
+                i -= 5
+            i += 1
+            
+        # Diagonal B
+        i = 0
+        j = y - 1
+        count_j = 0
+        count_i = 0
+        while count_i != x - 4:
+            while (i != x - 4):
+                while j != 4:
+                    while count_j != 5:
+                        if game.board[i][j] == '2':
+                            counter += 1
+                        i += 1
+                        j -= 1
+                        count_j += 1
+                    if (counter == 4):
+                        self.whichPattern = 4
+                        if self.findDefense(game, i, j) == True:
+                            return True
+                    counter = 0
+                    count_j = 0
+                    self.whichPattern = 0
+                    j += 4
+                    i -= 5
+                i += 1
+                j = y - 1
+            count_i += 1
         return False
 
     def getBestMove(self, game: Game, x, y):
